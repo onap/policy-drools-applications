495991903ed2a67c140010e1f468e758e7db4578
Ritu Sood <ritu.sood@intel.com>
2017-08-23 07:52:20 -0700

    [POLICY-57] Add VFC integration code
    This patch adds VFC Actor code and updates drool template for VFC.
    This is WIP patch. There are two opens for this patch that are
    still in discussion
    
    1) AA&I fields in onset event
    2) VFC URL, username and password
    
    Change-Id: Idc1d1fada295fa1c2e563ba37dd359f7b5c59f87
    Signed-off-by: Ritu Sood <ritu.sood@intel.com>

---
+++ controlloop/common/model-impl/vfc/src/main/java/org/onap/policy/vfc/VFCHealRequest.java
@@ -0,0 +1,42 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2017 Intel Corp. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.vfc;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import com.google.gson.annotations.SerializedName;
+
+public class VFCHealRequest implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@SerializedName("vnfInstanceId")
+	public String vnfInstanceId;
+
+	@SerializedName("cause")
+	public String cause;
+
+	@SerializedName("additionalParams")
+	public VFCHealAdditionalParams additionalParams;
+
+	public VFCHealRequest() {
+	}
+
+}
