c5f38f5de2f49d3f2fa7f03808f9f063f1cb2ed9
Hockla, Ali (ah999m) <ah999m@att.com>
2017-09-13 09:24:43 -0500

    Added changes for vDNS Use Case
    -Renamed remaining MSO classes and packages to SO and modified drl
    accordingly
    -Connected SO Interface to SO Simulator
    -Added vDNS Control Loop junit
    -Modified SOActorServiceProvider to reflect AAI class changes (POLICY-103)
    
    Issue-ID: POLICY-102
    
    Change-Id: Iea5d3f096c4ccfedde68e79d7593d66331127aaf
    Signed-off-by: Hockla, Ali (ah999m) <ah999m@att.com>

---
+++ controlloop/common/simulators/src/main/java/org/onap/policy/simulators/SoSimulatorJaxRs.java
@@ -0,0 +1,44 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * simulators
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.simulators;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+
+import org.onap.policy.so.SORequest;
+import org.onap.policy.so.util.Serialization;
+
+@Path("/serviceInstances")
+public class SoSimulatorJaxRs {
+	
+	@POST
+	@Path("/v2/{serviceInstanceId}/vnfs/{vnfInstanceId}/vfModulesHTTPS/1.1")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String SoPostQuery(@PathParam("serviceInstanceId") String serviceInstanceId, @PathParam("vnfInstanceId") String vnfInstanceId, String req)
+	{
+		SORequest request = Serialization.gsonPretty.fromJson(req, SORequest.class);
+		return "{\"requestReferences\": {\"instanceId\": \"ff305d54-75b4-ff1b-bdb2-eb6b9e5460ff\", \"requestId\": " + request.requestId + "}}";
+	}
+	
+}
