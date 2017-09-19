eb04e2e81ae3402fe7ad899f34495469b4c7514e
shashikanth <shashikanth.vh@huawei.com>
2017-09-19 14:57:58 +0530

    Fix Blocker/Critical sonar issues
    Fix Blocker/Critical sonar issues in policy/drools-applications module
    https://sonar.onap.org/component_issues?id=org.onap.policy.drools-applications%3Adrools-pdp-apps#resolved=false|severities=CRITICAL%2CMAJOR
    
    Fixed 2 issues,
    Used isEmpty() to check whether the collection is empty or not.
    getActor returns a string, there's no need to call toString()
    
    Issue-Id:POLICY-111
    Change-Id: I645420ee370ceb5fca959f870e76db2390e19929
    Signed-off-by: shashikanth.vh <shashikanth.vh@huawei.com>

--- controlloop/common/eventmanager/src/main/java/org/onap/policy/controlloop/eventmanager/ControlLoopOperationManager.java
+++ controlloop/common/eventmanager/src/main/java/org/onap/policy/controlloop/eventmanager/ControlLoopOperationManager.java
@@ -189,7 +189,7 @@ public class ControlLoopOperationManager implements Serializable {
 		this.policyResult = null;
 		Operation operation = new Operation();
 		operation.attempt = ++this.attempts;
-		operation.operation.actor = this.policy.getActor().toString();
+		operation.operation.actor = this.policy.getActor();
 		operation.operation.operation = this.policy.getRecipe();
 		operation.operation.target = this.policy.getTarget().toString();
 		operation.operation.subRequestId = Integer.toString(operation.attempt);
@@ -400,7 +400,8 @@ public class ControlLoopOperationManager implements Serializable {
 		if (this.currentOperation != null && this.currentOperation.operation != null) {
 			return this.currentOperation.operation.toMessage();
 		}
-		if (this.operationHistory != null && this.operationHistory.size() > 0) {
+
+		if (!this.operationHistory.isEmpty()) {
 			return this.operationHistory.getLast().operation.toMessage();
 		}
 		return null;
@@ -410,7 +411,8 @@ public class ControlLoopOperationManager implements Serializable {
 		if (this.currentOperation != null && this.currentOperation.operation != null) {
 			return this.currentOperation.operation.toMessage()+ ", Guard result: " + guardResult;
 		}
-		if (this.operationHistory != null && this.operationHistory.size() > 0) {
+          
+		if (!this.operationHistory.isEmpty()) {
 			return this.operationHistory.getLast().operation.toMessage() + ", Guard result: " + guardResult;
 		}
 		return null;
@@ -420,7 +422,8 @@ public class ControlLoopOperationManager implements Serializable {
 		if (this.currentOperation != null && this.currentOperation.operation != null) {
 			return this.currentOperation.operation.toHistory();
 		}
-		if (this.operationHistory != null && this.operationHistory.size() > 0) {
+          
+		if (!this.operationHistory.isEmpty()) {
 			return this.operationHistory.getLast().operation.toHistory();
 		}
 		return null;
