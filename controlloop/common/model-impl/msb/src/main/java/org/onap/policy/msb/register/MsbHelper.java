eb04465b036c15405a5791520d86d5125b264061
wangxinyuan10113332 <wang.xinyuan1@zte.com.cn>
2017-09-25 11:57:55 +0800

    Use MSB for service discovery
    Change-Id: If5713039ae3970ae703f4ccca5cab58bb45ec8a1
    Issue-ID: POLICY-172
    Signed-off-by: wangxinyuan10113332 <wang.xinyuan1@zte.com.cn>

---
+++ controlloop/common/model-impl/msb/src/main/java/org/onap/policy/msb/register/MsbHelper.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright 2017 ZTE, Inc. and others.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package org.onap.policy.msb.register;
+
+import org.onap.msb.sdk.discovery.common.RouteException;
+import org.onap.msb.sdk.discovery.entity.MicroServiceInfo;
+import org.onap.msb.sdk.discovery.entity.Node;
+import org.onap.msb.sdk.httpclient.msb.MSBServiceClient;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class MsbHelper {
+
+  private MSBServiceClient msbClient;
+
+  public MsbHelper(MSBServiceClient msbClient) {
+    super();
+    this.msbClient = msbClient;
+  }
+
+  public void registerMsb() throws UnknownHostException, RouteException {
+
+    MicroServiceInfo msinfo = new MicroServiceInfo();
+
+    msinfo.setServiceName("policy-pdp");
+    msinfo.setVersion("v1");
+    msinfo.setUrl("/api/policy/pdp/v1");
+    msinfo.setProtocol("REST");
+    msinfo.setVisualRange("0|1");
+    
+    Set<Node> nodes = new HashSet<>();
+    Node node1 = new Node();
+    node1.setIp(InetAddress.getLocalHost().getHostAddress());
+    node1.setPort("9090");
+    nodes.add(node1);
+    msinfo.setNodes(nodes);
+    msbClient.registerMicroServiceInfo(msinfo, false);
+  }
+}
