65500fbeb926f723336b515efabc4db07ef3d4a6
wangxinyuan10113332 <wang.xinyuan1@zte.com.cn>
2017-09-26 11:01:49 +0800

    Use MSB for service discovery
    Change-Id: I524946a1c352b5e9225b7fb37d35fa03a3f77e82
    Issue-ID: POLICY-172
    Signed-off-by: wangxinyuan10113332 <wang.xinyuan1@zte.com.cn>

---
+++ controlloop/common/msb/src/main/java/org/onap/policy/msb/client/MSBServiceFactory.java
@@ -0,0 +1,107 @@
+/*******************************************************************************
+ * Copyright 2017 ZTE, Inc. and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package org.onap.policy.msb.client;
+
+import org.onap.msb.sdk.discovery.common.RouteException;
+import org.onap.msb.sdk.discovery.entity.MicroServiceFullInfo;
+import org.onap.msb.sdk.discovery.entity.NodeInfo;
+import org.onap.msb.sdk.httpclient.msb.MSBServiceClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.Properties;
+
+
+public class MSBServiceFactory {
+    private static final Logger logger = LoggerFactory.getLogger(MSBServiceFactory.class);
+    private static final String msbPropertyFile = "msb.policy.properties";
+    private static final String MSB_IP = "msb.ip";
+    private static final String MSB_PORT = "msb.port";
+    private MSBServiceClient msbClient;
+    private Properties properties;
+
+    public MSBServiceFactory() throws MSBServiceException,IOException{
+        this.init();
+        this.msbClient = new MSBServiceClient(properties.getProperty(MSB_IP), Integer.parseInt(properties.getProperty(MSB_PORT)));
+    }
+    public MSBServiceFactory (MSBServiceClient msbClient) {
+        this.msbClient = msbClient;
+    }
+
+    private void init() throws MSBServiceException,IOException  {
+        properties = new Properties();
+        Path file = Paths.get(System.getProperty(msbPropertyFile));
+        if (file == null) {
+            throw new MSBServiceException("No msb.policy.properties specified.");
+        }
+        if (Files.notExists(file)) {
+            throw new MSBServiceException("No msb.policy.properties specified.");
+        }
+
+        if (Files.isReadable(file) == false) {
+            throw new MSBServiceException ("Repository is NOT readable: " + file.toAbsolutePath());
+        }
+        try(InputStream is = new FileInputStream(file.toFile())){
+            properties.load(is);
+        }
+    }
+
+
+    public Node getNode(String serviceName,String version){
+        return this.build(serviceName,version);
+    }
+
+    public Node getNode(String actor){
+        Node node = null;
+        switch (actor) {
+            case "AAI":
+                node = this.build("aai-search","v11");
+                return node;
+            case "SO":
+                node = this.build("so","v2");
+                return node;
+            case "VFC":
+                node = this.build("nfvo-nslcm","v1");
+                return node;
+            default:
+                logger.info("MSBServiceManager: policy has an unknown actor.");
+        }
+        return node;
+    }
+
+    private Node build(String serviceName,String version){
+        Node node = new Node();
+        node.setName(serviceName);
+        try {
+            MicroServiceFullInfo serviceInfo = msbClient.queryMicroServiceInfo(serviceName,version);
+            Iterator iterator = serviceInfo.getNodes().iterator();
+            while(iterator.hasNext()) {
+                NodeInfo nodeInfo = (NodeInfo)iterator.next();
+                node.setIp(nodeInfo.getIp());
+                node.setPort(nodeInfo.getPort());
+            }
+        } catch (RouteException e) {
+            logger.info("MSBServiceManager:",e);
+        }
+        return node;
+    }
+}
---
+++ controlloop/common/msb/src/test/java/org/onap/policy/msb/client/MSBServiceManagerTest.java
@@ -0,0 +1,116 @@
+/*******************************************************************************
+ * Copyright 2017 ZTE, Inc. and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package org.onap.policy.msb.client;
+
+import org.junit.*;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.onap.msb.sdk.discovery.common.RouteException;
+import org.onap.msb.sdk.discovery.entity.MicroServiceFullInfo;
+import org.onap.msb.sdk.discovery.entity.NodeInfo;
+import org.onap.msb.sdk.httpclient.msb.MSBServiceClient;
+import org.onap.policy.msb.client.MSBServiceManager;
+import org.onap.policy.msb.client.Node;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+public class MSBServiceManagerTest {
+    @Mock
+    private MSBServiceClient msbClient;
+
+    private MSBServiceManager msbManager;
+
+    public MSBServiceManagerTest(){}
+
+    @BeforeClass
+    public static void setUpClass(){}
+
+    @AfterClass
+    public static void tearDownClass(){}
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        msbManager = new MSBServiceManager(msbClient);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testByActor () throws RouteException,UnknownHostException {
+        MicroServiceFullInfo serviceInfo = this.build(InetAddress.getLocalHost().getHostAddress(),"8843");
+        when(msbClient.queryMicroServiceInfo("aai-search","v11")).thenReturn(serviceInfo);
+        Node node = msbManager.getNode("AAI");
+        assertNotNull(node);
+        serviceInfo = this.build(InetAddress.getLocalHost().getHostAddress(),"8840");
+        when(msbClient.queryMicroServiceInfo("so","v2")).thenReturn(serviceInfo);
+        node = msbManager.getNode("SO");
+        assertNotNull(node);
+
+        serviceInfo = this.build(InetAddress.getLocalHost().getHostAddress(),"8082");
+        when(msbClient.queryMicroServiceInfo("nfvo-nslcm","v1")).thenReturn(serviceInfo);
+        node = msbManager.getNode("VFC");
+        assertNotNull(node);
+
+    }
+
+    @Test
+    public void testByActor_when_actorNotExist_returnNull () throws RouteException,UnknownHostException {
+        MicroServiceFullInfo serviceInfo = this.build(InetAddress.getLocalHost().getHostAddress(),"8843");
+        when(msbClient.queryMicroServiceInfo("aai-search","v11")).thenReturn(serviceInfo);
+        Node node = msbManager.getNode("DDD");
+        assertNull(node);
+    }
+
+    @Test
+    public void testByServiceNameAndVersion () throws RouteException,UnknownHostException {
+        MicroServiceFullInfo serviceInfo = this.build(InetAddress.getLocalHost().getHostAddress(),"8843");
+        when(msbClient.queryMicroServiceInfo("aai-search","v11")).thenReturn(serviceInfo);
+        Node node = msbManager.getNode("aai-search","v11");
+        assertNotNull(node);
+    }
+
+    @Test
+    public void testByServiceNameAndVersion_when_serice_notRegistedToMSB () throws RouteException,UnknownHostException {
+        MicroServiceFullInfo serviceInfo = this.build(InetAddress.getLocalHost().getHostAddress(),"8843");
+        when(msbClient.queryMicroServiceInfo("aai-search","v11")).thenThrow(new RouteException());
+        Node node = msbManager.getNode("aai-search","v11");
+        assertNotNull(node);
+        assertTrue(node.getName() == "aai-search");
+        assertTrue(node.getIp() == null);
+        assertTrue(node.getPort() == null);
+    }
+
+    public static MicroServiceFullInfo build(String ip,String port){
+        MicroServiceFullInfo serviceInfo = new MicroServiceFullInfo();
+        Set<NodeInfo> nodes = new HashSet<NodeInfo>();
+        NodeInfo node= new NodeInfo();
+        node.setPort(port);
+        node.setIp(ip);
+        nodes.add(node);
+        serviceInfo.setNodes(nodes);
+        return serviceInfo;
+    }
+
+}
