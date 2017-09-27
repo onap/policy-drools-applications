eb253b9b09c7f8d548ab3d1589b12d47f1d29aaf
Hockla, Ali (ah999m) <ah999m@att.com>
2017-09-27 09:40:07 -0500

    Added changes for SO Request retries
    This change will allow the control loop to increment the operation
    attempts in the case of an SO failure in order to retry the
    operation if defined in the yaml.
    
    Issue-ID: POLICY-268
    
    Change-Id: I1fee2ce64524e58fc3068f19c5cf31ed91159e0f
    Signed-off-by: Hockla, Ali (ah999m) <ah999m@att.com>

--- controlloop/common/eventmanager/src/main/java/org/onap/policy/controlloop/eventmanager/ControlLoopOperationManager.java
+++ controlloop/common/eventmanager/src/main/java/org/onap/policy/controlloop/eventmanager/ControlLoopOperationManager.java
@@ -346,13 +346,25 @@ public class ControlLoopOperationManager implements Serializable {
 		    return null;
 		} else if (response instanceof SOResponse) {
 			SOResponse msoResponse = (SOResponse) response;
+			
+			Integer operationAttempt = null;
+			try {
+				operationAttempt = this.attempts;
+			} catch (Exception e) {
+				//
+				// We cannot tell what happened if this doesn't exist
+				//
+				this.completeOperation(operationAttempt, "Policy was unable to operation attempts (it was null).", PolicyResult.FAILURE_EXCEPTION);
+				return PolicyResult.FAILURE_EXCEPTION;
+			}
+
 			switch (msoResponse.httpResponseCode) {
 			case 200:
 			case 202:
 				//
 				// Consider it as success
 				//
-				this.completeOperation(new Integer(1), msoResponse.httpResponseCode + " Success", PolicyResult.SUCCESS);
+				this.completeOperation(operationAttempt, msoResponse.httpResponseCode + " Success", PolicyResult.SUCCESS);
 				if (this.policyResult != null && this.policyResult.equals(PolicyResult.FAILURE_TIMEOUT)) {
 					return null;
 				}
@@ -361,10 +373,12 @@ public class ControlLoopOperationManager implements Serializable {
 				//
 				// Consider it as failure
 				//
-				this.completeOperation(new Integer(1), msoResponse.httpResponseCode + " Failed", PolicyResult.FAILURE);
+				this.completeOperation(operationAttempt, msoResponse.httpResponseCode + " Failed", PolicyResult.FAILURE);
 				if (this.policyResult != null && this.policyResult.equals(PolicyResult.FAILURE_TIMEOUT)) {
 					return null;
 				}
+				// increment operation attempts for retries
+				this.attempts += 1;
 				return PolicyResult.FAILURE;
 			}
 
