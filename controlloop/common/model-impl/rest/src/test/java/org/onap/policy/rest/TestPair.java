c91bcc738f292dad9c1adfe72184ca821afdb451
vdmeer <sven.van.der.meer@ericsson.com>
2018-01-17 23:13:14 +0000

    Fix Technical Debt, basic JUnit tests for Rest
    Basic tests for get and post
    (not testing full user/pw/body)
    
    Issue-ID: POLICY-455
    Change-Id: I186ebf969abc239e5056199c2fd8da9f3253d2fc
    Signed-off-by: vdmeer.sven <sven.van.der.meer@ericsson.com>
    Signed-off-by: vdmeer <sven.van.der.meer@ericsson.com>

---
+++ controlloop/common/model-impl/rest/src/test/java/org/onap/policy/rest/TestPair.java
@@ -0,0 +1,42 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * rest
+ * ================================================================================
+ * 
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.rest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.onap.policy.rest.RESTManager.Pair;
+
+public class TestPair {
+
+    @Test
+    public void testPair() {
+        RESTManager mgr = new RESTManager();
+
+        Pair<Integer, Integer> pII = mgr.new Pair<>(1, 2);
+        assertEquals((Integer) 1, (Integer) pII.a);
+        assertEquals((Integer) 2, (Integer) pII.b);
+
+        Pair<Integer, String> pIS = mgr.new Pair<>(1, "test");
+        assertEquals((Integer) 1, (Integer) pIS.a);
+        assertEquals("test", pIS.b);
+    }
+}
