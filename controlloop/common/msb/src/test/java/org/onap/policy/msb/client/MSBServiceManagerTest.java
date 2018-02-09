23fca0124dc61ff67cae2e11171f0dcfc8995b1e
liamfallon <liam.fallon@ericsson.com>
2018-02-09 13:17:41 +0000

    Remove Technical Debt from MSB code
    Some minor warnings showed up in Eclipse on the MSB module. This change
    removes those warnings.
    
    Issue-ID: POLICY-455
    
    Change-Id: Ibdd8c563120178d73a55adfd0d8853b6bc82b820
    Signed-off-by: liamfallon <liam.fallon@ericsson.com>

--- controlloop/common/msb/src/test/java/org/onap/policy/msb/client/MSBServiceManagerTest.java
+++ controlloop/common/msb/src/test/java/org/onap/policy/msb/client/MSBServiceManagerTest.java
@@ -38,119 +38,118 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 public class MSBServiceManagerTest {
-    @Mock
-    private MSBServiceClient msbClient;
-    
-    @Rule
+	@Mock
+	private MSBServiceClient msbClient;
+
+	@Rule
 	public ExpectedException expectedException = ExpectedException.none();
 
-    private MSBServiceManager msbManager;
+	private MSBServiceManager msbManager;
 
-    public MSBServiceManagerTest(){}
+	public MSBServiceManagerTest(){}
 
-    @BeforeClass
-    public static void setUpClass(){}
+	@BeforeClass
+	public static void setUpClass(){}
 
-    @AfterClass
-    public static void tearDownClass(){}
+	@AfterClass
+	public static void tearDownClass(){}
 
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        msbManager = new MSBServiceManager(msbClient);
-    }
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		msbManager = new MSBServiceManager(msbClient);
+	}
 
-    @After
-    public void tearDown() {
-    }
+	@After
+	public void tearDown() {
+	}
 
-    @Test
-    public void testByActor () throws RouteException,UnknownHostException {
-        MicroServiceFullInfo serviceInfo = this.build(InetAddress.getLocalHost().getHostAddress(),"8843");
-        when(msbClient.queryMicroServiceInfo("aai-search","v11")).thenReturn(serviceInfo);
-        Node node = msbManager.getNode("AAI");
-        assertNotNull(node);
-        serviceInfo = this.build(InetAddress.getLocalHost().getHostAddress(),"8840");
-        when(msbClient.queryMicroServiceInfo("so","v2")).thenReturn(serviceInfo);
-        node = msbManager.getNode("SO");
-        assertNotNull(node);
+	@Test
+	public void testByActor () throws RouteException,UnknownHostException {
+		MicroServiceFullInfo serviceInfo = MSBServiceManagerTest.build(InetAddress.getLocalHost().getHostAddress(),"8843");
+		when(msbClient.queryMicroServiceInfo("aai-search","v11")).thenReturn(serviceInfo);
+		Node node = msbManager.getNode("AAI");
+		assertNotNull(node);
+		serviceInfo = MSBServiceManagerTest.build(InetAddress.getLocalHost().getHostAddress(),"8840");
+		when(msbClient.queryMicroServiceInfo("so","v2")).thenReturn(serviceInfo);
+		node = msbManager.getNode("SO");
+		assertNotNull(node);
 
-        serviceInfo = this.build(InetAddress.getLocalHost().getHostAddress(),"8082");
-        when(msbClient.queryMicroServiceInfo("nfvo-nslcm","v1")).thenReturn(serviceInfo);
-        node = msbManager.getNode("VFC");
-        assertNotNull(node);
+		serviceInfo = MSBServiceManagerTest.build(InetAddress.getLocalHost().getHostAddress(),"8082");
+		when(msbClient.queryMicroServiceInfo("nfvo-nslcm","v1")).thenReturn(serviceInfo);
+		node = msbManager.getNode("VFC");
+		assertNotNull(node);
 
-    }
+	}
 
-    @Test
-    public void testByActor_when_actorNotExist_returnNull () throws RouteException,UnknownHostException {
-        MicroServiceFullInfo serviceInfo = this.build(InetAddress.getLocalHost().getHostAddress(),"8843");
-        when(msbClient.queryMicroServiceInfo("aai-search","v11")).thenReturn(serviceInfo);
-        Node node = msbManager.getNode("DDD");
-        assertNull(node);
-    }
+	@Test
+	public void testByActor_when_actorNotExist_returnNull () throws RouteException,UnknownHostException {
+		MicroServiceFullInfo serviceInfo = MSBServiceManagerTest.build(InetAddress.getLocalHost().getHostAddress(),"8843");
+		when(msbClient.queryMicroServiceInfo("aai-search","v11")).thenReturn(serviceInfo);
+		Node node = msbManager.getNode("DDD");
+		assertNull(node);
+	}
 
-    @Test
-    public void testByServiceNameAndVersion () throws RouteException,UnknownHostException {
-        MicroServiceFullInfo serviceInfo = this.build(InetAddress.getLocalHost().getHostAddress(),"8843");
-        when(msbClient.queryMicroServiceInfo("aai-search","v11")).thenReturn(serviceInfo);
-        Node node = msbManager.getNode("aai-search","v11");
-        assertNotNull(node);
-    }
+	@Test
+	public void testByServiceNameAndVersion () throws RouteException,UnknownHostException {
+		MicroServiceFullInfo serviceInfo = MSBServiceManagerTest.build(InetAddress.getLocalHost().getHostAddress(),"8843");
+		when(msbClient.queryMicroServiceInfo("aai-search","v11")).thenReturn(serviceInfo);
+		Node node = msbManager.getNode("aai-search","v11");
+		assertNotNull(node);
+	}
 
-    @Test
-    public void testByServiceNameAndVersion_when_serice_notRegistedToMSB () throws RouteException,UnknownHostException {
-        MicroServiceFullInfo serviceInfo = this.build(InetAddress.getLocalHost().getHostAddress(),"8843");
-        when(msbClient.queryMicroServiceInfo("aai-search","v11")).thenThrow(new RouteException());
-        Node node = msbManager.getNode("aai-search","v11");
-        assertNotNull(node);
-        assertTrue(node.getName() == "aai-search");
-        assertTrue(node.getIp() == null);
-        assertTrue(node.getPort() == null);
-    }
-    
-    @Test
-    public void testReadMsbPolicyProperites_noPropertyFileSpecifed_throwsException() throws MSBServiceException, IOException {
-  	  expectedException.expect(MSBServiceException.class);
-  	  expectedException.expectMessage("No msb.policy.properties specified.");
-  	  System.clearProperty("msb.policy.properties");
-        msbManager = new MSBServiceManager();
-    }
-    
-    @Test 
-    public void testReadMsbPolicyProperites_propertyFileDoesNotExist_throwsException() throws MSBServiceException, IOException {
-  	  expectedException.expect(MSBServiceException.class);
-  	  expectedException.expectMessage("No msb.policy.properties specified.");
-  	  System.setProperty("msb.policy.properties", "nonExistingPropertyFile.txt");
-        msbManager = new MSBServiceManager();
-        System.clearProperty("msb.policy.properties");
-    }
-    
-    @Test 
-    public void testReadMsbPolicyProperites_propertyFileExists() throws MSBServiceException, IOException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-  	  System.setProperty("msb.policy.properties", "src/test/resources/msbPropertyFile.properties");
-      msbManager = new MSBServiceManager();
-      System.clearProperty("msb.policy.properties");
-      
-      Field factoryField = msbManager.getClass().getDeclaredField("factory");
-      factoryField.setAccessible(true);
-      MSBServiceFactory msbServiceFactory = (MSBServiceFactory) factoryField.get(msbManager);
-      
-      Field msbClientField = msbServiceFactory.getClass().getDeclaredField("msbClient");
-      msbClientField.setAccessible(true);
-      MSBServiceClient msbClient = (MSBServiceClient) msbClientField.get(msbServiceFactory);
-      assertEquals("127.0.0.1:20", msbClient.getMsbSvrAddress());
-    }
+	@Test
+	public void testByServiceNameAndVersion_when_serice_notRegistedToMSB () throws RouteException,UnknownHostException {
+		MSBServiceManagerTest.build(InetAddress.getLocalHost().getHostAddress(),"8843");
+		when(msbClient.queryMicroServiceInfo("aai-search","v11")).thenThrow(new RouteException());
+		Node node = msbManager.getNode("aai-search","v11");
+		assertNotNull(node);
+		assertTrue(node.getName() == "aai-search");
+		assertTrue(node.getIp() == null);
+		assertTrue(node.getPort() == null);
+	}
 
-    public static MicroServiceFullInfo build(String ip,String port){
-        MicroServiceFullInfo serviceInfo = new MicroServiceFullInfo();
-        Set<NodeInfo> nodes = new HashSet<NodeInfo>();
-        NodeInfo node= new NodeInfo();
-        node.setPort(port);
-        node.setIp(ip);
-        nodes.add(node);
-        serviceInfo.setNodes(nodes);
-        return serviceInfo;
-    }
+	@Test
+	public void testReadMsbPolicyProperites_noPropertyFileSpecifed_throwsException() throws MSBServiceException, IOException {
+		expectedException.expect(MSBServiceException.class);
+		expectedException.expectMessage("No msb.policy.properties specified.");
+		System.clearProperty("msb.policy.properties");
+		msbManager = new MSBServiceManager();
+	}
 
+	@Test 
+	public void testReadMsbPolicyProperites_propertyFileDoesNotExist_throwsException() throws MSBServiceException, IOException {
+		expectedException.expect(MSBServiceException.class);
+		expectedException.expectMessage("No msb.policy.properties specified.");
+		System.setProperty("msb.policy.properties", "nonExistingPropertyFile.txt");
+		msbManager = new MSBServiceManager();
+		System.clearProperty("msb.policy.properties");
+	}
+
+	@Test 
+	public void testReadMsbPolicyProperites_propertyFileExists() throws MSBServiceException, IOException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		System.setProperty("msb.policy.properties", "src/test/resources/msbPropertyFile.properties");
+		msbManager = new MSBServiceManager();
+		System.clearProperty("msb.policy.properties");
+
+		Field factoryField = msbManager.getClass().getDeclaredField("factory");
+		factoryField.setAccessible(true);
+		MSBServiceFactory msbServiceFactory = (MSBServiceFactory) factoryField.get(msbManager);
+
+		Field msbClientField = msbServiceFactory.getClass().getDeclaredField("msbClient");
+		msbClientField.setAccessible(true);
+		MSBServiceClient msbClient = (MSBServiceClient) msbClientField.get(msbServiceFactory);
+		assertEquals("127.0.0.1:20", msbClient.getMsbSvrAddress());
+	}
+
+	public static MicroServiceFullInfo build(String ip,String port){
+		MicroServiceFullInfo serviceInfo = new MicroServiceFullInfo();
+		Set<NodeInfo> nodes = new HashSet<NodeInfo>();
+		NodeInfo node= new NodeInfo();
+		node.setPort(port);
+		node.setIp(ip);
+		nodes.add(node);
+		serviceInfo.setNodes(nodes);
+		return serviceInfo;
+	}
 }
