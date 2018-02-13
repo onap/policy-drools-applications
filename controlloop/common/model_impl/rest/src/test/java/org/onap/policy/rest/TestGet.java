8b4a90d68ac5d19402d246d5f6ed6f3b08e6883f
liamfallon <liam.fallon@ericsson.com>
2018-02-13 11:29:56 +0000

    Rename maven modules to mirror directory structure
    The naming of Maven modules in drools-applications was not
    aligned with the directory structure in the git repository
    of drools-applications. Therefore it was difficult to
    see the strucutre of the repository in Eclipse and other
    IDEs. THis change amends the Maven module IDs to
    reflect the repository directory structure.
    
    Issue-ID: POLICY-238
    
    Change-Id: I8ab1a7ecdb664045222bbbfda269135e3e449109
    Signed-off-by: liamfallon <liam.fallon@ericsson.com>

---
+++ controlloop/common/model_impl/rest/src/test/java/org/onap/policy/rest/TestGet.java
@@ -0,0 +1,52 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * rest
+ * ================================================================================
+ * 
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.rest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.onap.policy.rest.RESTManager.Pair;
+
+public class TestGet {
+
+    @Test(expected = NullPointerException.class)
+    public void testUrlNull() {
+        RESTManager mgr = new RESTManager();
+        mgr.get(null, "user", null, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUsernameNull() {
+        RESTManager mgr = new RESTManager();
+        mgr.get("nothing", null, null, null);
+    }
+
+    @Test
+    public void testUrlExampleOrg() {
+        RESTManager mgr = new RESTManager();
+
+        Pair<Integer, String> result = mgr.get("http://www.example.org", "user", null, null);
+        assertEquals((Integer)200, result.a);
+        assertTrue(result.b != null);
+        assertTrue(result.b.length() > 0);
+    }
+}
