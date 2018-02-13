9adf5f60f5fa49bf4e04458b8979dff5db431f06
liamfallon <liam.fallon@ericsson.com>
2018-02-13 11:29:56 +0000

    Rename maven modules to mirror directory structure
    The naming of Maven modules in drools-applications was not
    aligned with the directory structure in the git repository
    of drools-applications. Therefore it was difficult to
    see the strucutre of the repository in Eclipse and other
    IDEs. THis change amends the Maven module IDs to
    reflect the repository directory structure.
    
    Issue-ID: POLICY-238
    Change-Id: I8ab1a7ecdb664045222bbbfda269135e3e449109
    Signed-off-by: liamfallon <liam.fallon@ericsson.com>

---
+++ controlloop/common/model_impl/so/src/main/java/org/onap/policy/so/SORequestParameters.java
@@ -0,0 +1,60 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * so
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.so;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.annotations.SerializedName;
+
+public class SORequestParameters implements Serializable {
+
+    private static final long serialVersionUID = -3283942659786236032L;
+
+    @SerializedName("subscriptionServiceType")
+    private String subscriptionServiceType;
+
+    @SerializedName("userParams")
+    private List<Map<String, String>> userParams = new LinkedList<>();
+
+    public SORequestParameters() {
+        // required by author
+    }
+
+    public String getSubscriptionServiceType() {
+        return subscriptionServiceType;
+    }
+
+    public List<Map<String, String>> getUserParams() {
+        return userParams;
+    }
+
+    public void setSubscriptionServiceType(String subscriptionServiceType) {
+        this.subscriptionServiceType = subscriptionServiceType;
+    }
+
+    public void setUserParams(List<Map<String, String>> userParams) {
+        this.userParams = userParams;
+    }
+
+}
