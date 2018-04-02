b60baa7e45bc12962f8f97011df0e9308a51a880
Jorge Hernandez <jh1730@att.com>
2018-04-02 17:23:47 -0500

    installation enhancements
    The following changes aim to have a quicker start of the drools container:
    
    1.  For both amsterdam (and the experimental beijing) controllers
        dependencies are pre-installed, so the loading of 3rd party
        dependencies are faster.
    
    2.  Further enhancements in installation.
    
    3.  Make sure that the naming of the generated control loop
        artifacts have a "control loop" label associated with it,
        so in the future, if other applications are added (non
        control loop related) do not conflict and is clear.
    
    Change-Id: Iecb84d186fcc34069aa5c4a175a8a4521b38499d
    Issue-ID: POLICY-534
    Signed-off-by: Jorge Hernandez <jh1730@att.com>

---
+++ controlloop/common/feature-controlloop-beijing/src/main/java/org/onap/policy/drools/apps/controlloop/feature/beijing/BeijingFeature.java
@@ -0,0 +1,48 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.drools.apps.controlloop.feature.beijing;
+
+import org.onap.policy.drools.features.PolicyEngineFeatureAPI;
+
+/**
+ * Beijing installation as a feature saves time
+ * loading the Beijing controller at runtime over the
+ * usual installation from nexus.  It also reduces
+ * potential for errors in the pom.xml generated in
+ * the brmsgw side.
+ *
+ * There is no impact on other components as the brmsgw
+ * etc .. they will continue operating as usual.
+ *
+ * This class will be expanded in the future for additional
+ * functionality
+ *
+ */
+public class BeijingFeature implements PolicyEngineFeatureAPI {
+
+    public static final int SEQNO = 1000;
+
+    @Override
+	public int getSequenceNumber() {
+		return SEQNO;
+	}
+
+}
---
+++ controlloop/packages/artifacts-controlloop/src/files/README.controlloop.artifacts.txt
@@ -0,0 +1,2 @@
+The artifacts directory contains Control Loop Application Dependencies that are
+to be installed in the local nexus instance.
