a29f33e7fa5565ad5bc89bb5d05498eaf0dc18d8
Saravanan A <saravanan.a75@wipro.com>
2018-09-10 17:31:33 +0530

    Add implementation for OOF PCI use case
    Receive DMaaP message from PCI-Handler MS with PCI Config
    change recommendations through DCAE_CL_OUTPUT topic.
    
    Trigger SDN-R (if allowed by policy) by sending
    DMaaP request through SDNR-CL topic.
    
    When the response is received from SDNR through
    SDNR-CL-RSP topic, just parse and print.
    
    Code review comments addressed
    
    Change-Id: If340a23ae18367b7f98e31fe79c09a09e645b2ad
    Issue-ID: POLICY-1089
    Signed-off-by: Saravanan A<saravanan.a75@wipro.com>

--- controlloop/common/model-impl/events/src/main/java/org/onap/policy/controlloop/ControlLoopEvent.java
+++ controlloop/common/model-impl/events/src/main/java/org/onap/policy/controlloop/ControlLoopEvent.java
@@ -20,10 +20,10 @@
 
 package org.onap.policy.controlloop;
 
-import com.google.gson.annotations.SerializedName;
-
 import java.io.Serializable;
 import java.util.UUID;
+
+import com.google.gson.annotations.SerializedName;
 
 public abstract class ControlLoopEvent implements Serializable {
 
@@ -62,6 +62,9 @@ public abstract class ControlLoopEvent implements Serializable {
     @SerializedName("closedLoopEventStatus")
     private ControlLoopEventStatus closedLoopEventStatus;
 
+    @SerializedName("payload")
+    private String payload;
+    
     public ControlLoopEvent() {
 
     }
@@ -85,6 +88,7 @@ public abstract class ControlLoopEvent implements Serializable {
         this.policyName = event.policyName;
         this.policyVersion = event.policyVersion;
         this.closedLoopEventStatus = event.closedLoopEventStatus;
+        this.payload = event.payload;
     }
 
     public boolean isEventStatusValid() {
@@ -177,5 +181,13 @@ public abstract class ControlLoopEvent implements Serializable {
 
     public void setClosedLoopEventStatus(ControlLoopEventStatus closedLoopEventStatus) {
         this.closedLoopEventStatus = closedLoopEventStatus;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload;
     }
 }
