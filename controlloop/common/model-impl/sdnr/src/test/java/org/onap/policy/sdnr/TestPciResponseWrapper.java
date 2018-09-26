9468817ac4bfb9777d50d6bebe0a9755a7c15962
Pamela Dragosh <pdragosh@research.att.com>
2018-09-26 13:53:38 -0400

    Fix for checkstyle declarations
    I found the problem with policy-yaml. An exception was
    occuring due to an extra line between an @return for
    javadoc vs. just printing a warning.
    
    See: https://github.com/checkstyle/checkstyle/issues/2398
    
    I also messed up some of the other declarations in the
    sub-modules.
    
    Copyright
    
    Issue-ID: POLICY-1153
    Change-Id: I848e0b19340d494775e161bab86ffb60a97d83ab
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

--- controlloop/common/model-impl/sdnr/src/test/java/org/onap/policy/sdnr/TestPciResponseWrapper.java
+++ controlloop/common/model-impl/sdnr/src/test/java/org/onap/policy/sdnr/TestPciResponseWrapper.java
@@ -26,8 +26,6 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import com.google.gson.Gson;
-
 import org.junit.Test;
 
 public class TestPciResponseWrapper {
