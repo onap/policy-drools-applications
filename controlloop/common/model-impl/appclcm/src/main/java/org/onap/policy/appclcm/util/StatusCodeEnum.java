7726e959695b32110deaf6f0e2a24abd373fda15
Krishnakumar Jinka <kris.jinka@samsung.com>
2018-11-25 13:56:36 +0900

    Reduce resp values meth complexity
    The response values determining method has more complexity
    reduce it using enums. Fix build fail due to incorrect
    response value. Modify LCM response code to enum. Fix
    enum package
    
    Issue-ID: POLICY-1251
    Change-Id: I49909b6dd2810dd864784afab0a328b855131501
    Signed-off-by: kris.jinka <kris.jinka@samsung.com>

---
+++ controlloop/common/model-impl/appclcm/src/main/java/org/onap/policy/appclcm/util/StatusCodeEnum.java
@@ -0,0 +1,54 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2018 Samsung Electronics Co., Ltd. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.appclcm.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum StatusCodeEnum {
+
+    ACCEPTED(100, "ACCEPTED"), SUCCESS(200, "ERROR"), ERROR(400, "SUCCESS"),
+    PARTIAL_SUCCESS(500, "PARTIAL SUCCESS");
+
+    private int code;
+    private String status;
+
+    private static Map<Integer, StatusCodeEnum> map = new HashMap<Integer, StatusCodeEnum>();
+
+    static {
+        for (StatusCodeEnum statusEnum : StatusCodeEnum.values()) {
+            map.put(statusEnum.code, statusEnum);
+        }
+    }
+
+    StatusCodeEnum(final int val, final String status) {
+        code = val;
+        this.status = status;
+    }
+
+    public static StatusCodeEnum valueOf(int val) {
+        return map.get(val);
+    }
+
+    public String toString() {
+        return this.status;
+    }
+}
