c57ffc3e48dc726b7c382377aec8175dedecca1b
pramod.jamkhedkar <pramod@research.att.com>
2019-03-20 16:29:02 -0400

    Create Response Object
    Created Custom Query response object from sample A&AI custom query
    output, Exceptions handled.
    
    Issue-ID: POLICY-1266
    Change-Id: I99e702274a42ff8141b87d68f53aa81e8600bbd5
    Signed-off-by: pramod.jamkhedkar <pramod@research.att.com>

---
+++ controlloop/common/model-impl/aai/src/test/java/org/onap/policy/aai/AaiCqResponseTest.java
@@ -0,0 +1,219 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.aai;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import org.onap.aai.domain.yang.v15.GenericVnf;
+import org.onap.aai.domain.yang.v15.ServiceInstance;
+import org.onap.aai.domain.yang.v15.VfModule;
+import org.onap.aai.domain.yang.v15.Vserver;
+import org.onap.policy.aai.util.Serialization;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AaiCqResponseTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AaiCqResponseTest.class);
+    private static final String CQ_RESPONSE_SAMPLE = "src/test/resources/org/onap/policy/aai/AaiCqResponse.json";
+
+
+    @Test
+    public void testConstructor() throws Exception {
+        /*
+         * Read JSON String and add all AaiObjects
+         */
+
+        String responseString = "";
+        responseString = new String(Files.readAllBytes(new File(CQ_RESPONSE_SAMPLE).toPath()));
+
+        AaiCqResponse aaiCqResponse;
+        aaiCqResponse = new AaiCqResponse(responseString);
+        assertNotNull(aaiCqResponse);
+        assertNotNull(aaiCqResponse.getInventoryResponseItems());
+        LOGGER.info(Serialization.gsonPretty.toJson(aaiCqResponse));
+
+
+    }
+
+    @Test
+    public void testAaiMalformedCqResponse() throws Exception {
+        /*
+         * Read JSON String and add all AaiObjects
+         */
+
+        String responseString = "";
+        responseString = new String(Files
+                .readAllBytes(new File("src/test/resources/org/onap/policy/aai/AaiMalformedCqResponse.json").toPath()));
+
+        AaiCqResponse aaiCqResponse;
+        aaiCqResponse = new AaiCqResponse(responseString);
+        for (Object aaiObj : aaiCqResponse.getInventoryResponseItems()) {
+            assertNull(aaiObj);
+        }
+        LOGGER.info(Serialization.gsonPretty.toJson(aaiCqResponse));
+
+
+    }
+
+    @Test
+    public void testGetItemByList() throws Exception {
+        /*
+         * Read JSON String and add all AaiObjects
+         */
+
+        String responseString = "";
+        responseString = new String(Files.readAllBytes(new File(CQ_RESPONSE_SAMPLE).toPath()));
+
+        AaiCqResponse aaiCqResponse;
+        aaiCqResponse = new AaiCqResponse(responseString);
+        ArrayList<Vserver> vs = (ArrayList<Vserver>) aaiCqResponse.getItemListByType(Vserver.class);
+        assertNotNull(vs);
+        LOGGER.info(vs.get(0).getVserverId());
+
+    }
+
+    @Test
+    public void testGetServiceInstance() throws Exception {
+
+        String responseString = "";
+        responseString = new String(Files.readAllBytes(new File(CQ_RESPONSE_SAMPLE).toPath()));
+
+        AaiCqResponse aaiCqResponse;
+        aaiCqResponse = new AaiCqResponse(responseString);
+        ServiceInstance si = aaiCqResponse.getServiceInstance();
+        assertNotNull(si);
+        LOGGER.info(si.getServiceInstanceName());
+    }
+
+
+
+    @Test
+    public void testGetGenericVnfs() throws Exception {
+
+        String responseString = "";
+        responseString = new String(Files.readAllBytes(new File(CQ_RESPONSE_SAMPLE).toPath()));
+
+        AaiCqResponse aaiCqResponse;
+        aaiCqResponse = new AaiCqResponse(responseString);
+        List<GenericVnf> genericVnfList = aaiCqResponse.getGenericVnfs();
+        assertNotNull(genericVnfList);
+        for (GenericVnf genVnf : genericVnfList) {
+            LOGGER.info(genVnf.getVnfName());
+        }
+
+    }
+
+
+
+    @Test
+    public void testGetDefaultGenericVnf() throws Exception {
+
+        String responseString = "";
+        responseString = new String(Files.readAllBytes(new File(CQ_RESPONSE_SAMPLE).toPath()));
+
+        AaiCqResponse aaiCqResponse;
+        aaiCqResponse = new AaiCqResponse(responseString);
+        GenericVnf genVnf = aaiCqResponse.getDefaultGenericVnf();
+        assertNotNull(genVnf);
+        LOGGER.info(genVnf.getVnfName());
+
+    }
+
+    @Test
+    public void testGetGenericVnfByName() throws Exception {
+
+        String responseString = "";
+        responseString = new String(Files.readAllBytes(new File(CQ_RESPONSE_SAMPLE).toPath()));
+
+        AaiCqResponse aaiCqResponse;
+        aaiCqResponse = new AaiCqResponse(responseString);
+        GenericVnf genVnf = aaiCqResponse.getGenericVnfByVnfName("TestVM-Vnf-0201-1");
+        assertNotNull(genVnf);
+        LOGGER.info(genVnf.getVnfName());
+    }
+
+
+    @Test
+    public void testGetAllVfModules() throws Exception {
+
+        String responseString = "";
+        responseString = new String(Files.readAllBytes(new File(CQ_RESPONSE_SAMPLE).toPath()));
+
+        AaiCqResponse aaiCqResponse;
+        aaiCqResponse = new AaiCqResponse(responseString);
+        List<VfModule> vfModuleList = aaiCqResponse.getAllVfModules();
+        assertNotNull(vfModuleList);
+        for (VfModule vfMod : vfModuleList) {
+            LOGGER.info(vfMod.getVfModuleName());
+        }
+
+    }
+
+
+    @Test
+    public void testGetVfModuleByVfModuleName() throws Exception {
+
+        String responseString = "";
+        responseString = new String(Files.readAllBytes(new File(CQ_RESPONSE_SAMPLE).toPath()));
+
+        AaiCqResponse aaiCqResponse;
+        aaiCqResponse = new AaiCqResponse(responseString);
+        VfModule vfModule = aaiCqResponse.getVfModuleByVfModuleName("vLoadBalancerMS-0211-1");
+        assertNotNull(vfModule);
+        LOGGER.info(vfModule.getVfModuleName());
+
+
+    }
+
+    @Test
+    public void testGetDefaultVfModule() throws Exception {
+
+        String responseString = "";
+        responseString = new String(Files.readAllBytes(new File(CQ_RESPONSE_SAMPLE).toPath()));
+
+        AaiCqResponse aaiCqResponse;
+        aaiCqResponse = new AaiCqResponse(responseString);
+        VfModule vfModule = aaiCqResponse.getDefaultVfModule();
+        assertNotNull(vfModule);
+        LOGGER.info(vfModule.getVfModuleName());
+    }
+
+    @Test
+    public void testGetVserver() throws Exception {
+
+        String responseString = "";
+        responseString = new String(Files.readAllBytes(new File(CQ_RESPONSE_SAMPLE).toPath()));
+
+        AaiCqResponse aaiCqResponse;
+        aaiCqResponse = new AaiCqResponse(responseString);
+        Vserver vserver = aaiCqResponse.getVserver();
+        assertNotNull(vserver);
+        LOGGER.info(vserver.getVserverName());
+
+    }
+
+}
