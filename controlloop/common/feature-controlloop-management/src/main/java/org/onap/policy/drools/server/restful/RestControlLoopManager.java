9a4e3a9ac092af19795d9696b3d9f02ce4f50cf3
Jim Hahn <jrh3@att.com>
2019-08-05 14:17:01 -0400

    Fix sonar issues in drools-applications
    Addressed sonar issue, "Move constants to a class or enum.", by
    moving the "manager" object from the ControlLoopMetrics interface
    into a utility class, ControlLoopMetricsManager.
    Addressed sonar issue, "duplicated blocks of code must be removed.",
    by refactoring PolicyGuardYamlToXacml, extracing common functions.
    Addressed sonar issue, "Remove this unused import", in
    RestControlLoopManager.
    Addressed sonar issue, "Refactor this method to throw at most one
    checked exception", in event manager.
    Addressed reviewer comment:
      Use "replace" instead of "replaceAll", thus avoiding escaping
    
    Change-Id: I47db957c83c1b3e2bd2330474e261987c6f0aac6
    Issue-ID: POLICY-1967
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- controlloop/common/feature-controlloop-management/src/main/java/org/onap/policy/drools/server/restful/RestControlLoopManager.java
+++ controlloop/common/feature-controlloop-management/src/main/java/org/onap/policy/drools/server/restful/RestControlLoopManager.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * ONAP
  * ================================================================================
- * Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2018-2019 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,7 +41,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import org.onap.policy.aai.AaiManager;
-import org.onap.policy.aai.util.AaiException;
 import org.onap.policy.controlloop.ControlLoopException;
 import org.onap.policy.controlloop.eventmanager.ControlLoopEventManager;
 import org.onap.policy.controlloop.params.ControlLoopParams;
