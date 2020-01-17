9bb84e47187fef94e028ff9dc374355773205bd3
Pamela Dragosh <pdragosh@research.att.com>
2020-01-17 07:55:30 -0500

    Update m2 test to mockito2 for JDK 11
    Peyton's review was pulled in before Ali's JDK 11 fix. So
    unfortunately this update did not get done.
    
    Issue-ID: POLICY-1587
    Change-Id: I9666b6e2f2327183e15bfe19d34515d0426c8ff0
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

--- controlloop/m2/util/src/test/java/org/onap/policy/util/DroolsSessionCommonSerializableTest.java
+++ controlloop/m2/util/src/test/java/org/onap/policy/util/DroolsSessionCommonSerializableTest.java
@@ -21,7 +21,7 @@
 package org.onap.policy.util;
 
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
