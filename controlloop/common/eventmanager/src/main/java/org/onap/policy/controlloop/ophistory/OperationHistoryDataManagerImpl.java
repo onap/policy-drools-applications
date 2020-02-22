c63e190bf4cf9dfd106755aa563786857162c643
Jim Hahn <jrh3@att.com>
2020-02-22 17:11:20 -0500

    Add frankfurt rules for Actor redesign
    Note: the VcpeTest is not included, as the APPC-LCM Restart
    operation is not available yet.
    
    Issue-ID: POLICY-2385
    Signed-off-by: Jim Hahn <jrh3@att.com>
    Change-Id: Ib4a4d75461c734ae47309e41dc9d099e8815d55d

---
+++ controlloop/common/eventmanager/src/main/java/org/onap/policy/controlloop/ophistory/OperationHistoryDataManagerImpl.java
@@ -0,0 +1,290 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.controlloop.ophistory;
+
+import java.util.Date;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.function.Consumer;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import lombok.Getter;
+import org.apache.commons.lang3.tuple.Triple;
+import org.eclipse.persistence.config.PersistenceUnitProperties;
+import org.onap.policy.common.parameters.ValidationResult;
+import org.onap.policy.common.utils.jpa.EntityMgrCloser;
+import org.onap.policy.common.utils.jpa.EntityTransCloser;
+import org.onap.policy.controlloop.ControlLoopOperation;
+import org.onap.policy.controlloop.VirtualControlLoopEvent;
+import org.onap.policy.database.operationshistory.Dbao;
+import org.onap.policy.guard.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Data manager that stores records in the DB, asynchronously, using a background thread.
+ */
+public class OperationHistoryDataManagerImpl implements OperationHistoryDataManager {
+    private static final Logger logger = LoggerFactory.getLogger(OperationHistoryDataManagerImpl.class);
+
+    /**
+     * Added to the end of {@link #operations} when {@link #stop()} is called. This is
+     * used to get the background thread out of a blocking wait for the next record.
+     */
+    private static final Triple<String, VirtualControlLoopEvent, ControlLoopOperation> END_MARKER =
+                    Triple.of(null, null, null);
+
+    // copied from the parameters
+    private final int maxQueueLength;
+    private final int batchSize;
+
+    private final EntityManagerFactory emFactory;
+
+    /**
+     * Thread that takes records from {@link #operations} and stores them in the DB.
+     */
+    private Thread thread;
+
+    /**
+     * Set to {@code true} to stop the background thread.
+     */
+    private boolean stopped = false;
+
+    /**
+     * Queue of operations waiting to be stored in the DB. When {@link #stop()} is called,
+     * an {@link #END_MARKER} is added to the end of the queue.
+     */
+    private final BlockingQueue<Triple<String, VirtualControlLoopEvent, ControlLoopOperation>> operations =
+                    new LinkedBlockingQueue<>();
+
+    /**
+     * Number of records that have been added to the DB by this data manager instance.
+     */
+    @Getter
+    private long recordsAdded = 0;
+
+
+    /**
+     * Constructs the object.
+     *
+     * @param params data manager parameters
+     */
+    public OperationHistoryDataManagerImpl(OperationHistoryDataManagerParams params) {
+        ValidationResult result = params.validate("data-manager-properties");
+        if (!result.isValid()) {
+            throw new IllegalArgumentException(result.getResult());
+        }
+
+        maxQueueLength = params.getMaxQueueLength();
+        batchSize = params.getBatchSize();
+
+        // create the factory using the properties
+        Properties props = toProperties(params);
+        this.emFactory = makeEntityManagerFactory(params.getPersistenceUnit(), props);
+    }
+
+    @Override
+    public synchronized void start() {
+        if (stopped || thread != null) {
+            // already started
+            return;
+        }
+
+        thread = makeThread(emFactory, this::run);
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+    @Override
+    public synchronized void stop() {
+        stopped = true;
+
+        if (thread == null) {
+            // no thread to close the factory - do it here
+            emFactory.close();
+
+        } else {
+            // the thread will close the factory when it sees the end marker
+            operations.add(END_MARKER);
+        }
+    }
+
+    @Override
+    public synchronized void store(String requestId, VirtualControlLoopEvent event, ControlLoopOperation operation) {
+
+        if (stopped) {
+            logger.warn("operation history thread is stopped, discarding requestId={} event={} operation={}", requestId,
+                            event, operation);
+            return;
+        }
+
+        operations.add(Triple.of(requestId, event, operation));
+
+        if (operations.size() > maxQueueLength) {
+            Triple<String, VirtualControlLoopEvent, ControlLoopOperation> discarded = operations.remove();
+            logger.warn("too many items to store in the operation history table, "
+                            + "discarding requestId={} event={} operation={}", discarded.getLeft(),
+                            discarded.getMiddle(), discarded.getRight());
+        }
+    }
+
+    /**
+     * Takes records from {@link #operations} and stores them in the queue. Continues to
+     * run until {@link #stop()} is invoked, or the thread is interrupted.
+     *
+     * @param emfactory entity manager factory
+     */
+    private void run(EntityManagerFactory emfactory) {
+        try {
+            // store records until stopped, continuing if an exception occurs
+            while (!stopped) {
+                try {
+                    Triple<String, VirtualControlLoopEvent, ControlLoopOperation> triple = operations.take();
+                    storeBatch(emfactory.createEntityManager(), triple);
+
+                } catch (RuntimeException e) {
+                    logger.error("failed to save data to operation history table", e);
+
+                } catch (InterruptedException e) {
+                    logger.error("interrupted, discarding remaining operation history data", e);
+                    Thread.currentThread().interrupt();
+                    return;
+                }
+            }
+
+            operations.add(END_MARKER);
+            storeRemainingRecords(emfactory);
+
+        } finally {
+            synchronized (this) {
+                stopped = true;
+            }
+
+            emfactory.close();
+        }
+    }
+
+    /**
+     * Store any remaining records, but stop at the first exception.
+     *
+     * @param emfactory entity manager factory
+     */
+    private void storeRemainingRecords(EntityManagerFactory emfactory) {
+        try {
+            while (operations.peek() != END_MARKER) {
+                storeBatch(emfactory.createEntityManager(), operations.poll());
+
+                // add another end marker, just in case storeBatch() discarded it
+                operations.add(END_MARKER);
+            }
+
+        } catch (RuntimeException e) {
+            logger.error("failed to save remaining data to operation history table", e);
+        }
+    }
+
+    /**
+     * Stores a batch of records.
+     *
+     * @param entityManager entity manager
+     * @param firstTriple first record to be stored
+     */
+    private void storeBatch(EntityManager entityManager,
+                    Triple<String, VirtualControlLoopEvent, ControlLoopOperation> firstTriple) {
+
+        try (EntityMgrCloser emc = new EntityMgrCloser(entityManager);
+                        EntityTransCloser trans = new EntityTransCloser(entityManager.getTransaction())) {
+
+            int nrecords = 0;
+            Triple<String, VirtualControlLoopEvent, ControlLoopOperation> triple = firstTriple;
+
+            while (triple != null && triple != END_MARKER) {
+                storeRecord(entityManager, triple.getLeft(), triple.getMiddle(), triple.getRight());
+
+                if (++nrecords >= batchSize) {
+                    break;
+                }
+
+                triple = operations.poll();
+            }
+
+            trans.commit();
+            recordsAdded += nrecords;
+        }
+    }
+
+    /**
+     * Stores a record.
+     *
+     * @param entityManager entity manager
+     * @param requestId request ID
+     * @param event event with which the operation is associated
+     * @param operation operation to be stored
+     */
+    private void storeRecord(EntityManager entityMgr, String requestId, VirtualControlLoopEvent event,
+                    ControlLoopOperation operation) {
+
+        Dbao newEntry = new Dbao();
+
+        newEntry.setClosedLoopName(event.getClosedLoopControlName());
+        newEntry.setRequestId(requestId);
+        newEntry.setActor(operation.getActor());
+        newEntry.setOperation(operation.getOperation());
+        newEntry.setTarget(operation.getTarget());
+        newEntry.setSubrequestId(operation.getSubRequestId());
+        newEntry.setMessage(operation.getMessage());
+        newEntry.setOutcome(operation.getOutcome());
+        if (operation.getStart() != null) {
+            newEntry.setStarttime(new Date(operation.getStart().toEpochMilli()));
+        }
+        if (operation.getEnd() != null) {
+            newEntry.setEndtime(new Date(operation.getEnd().toEpochMilli()));
+        }
+
+        entityMgr.persist(newEntry);
+    }
+
+    /**
+     * Converts the parameters to Properties.
+     *
+     * @param params parameters to be converted
+     * @return a new property set
+     */
+    private Properties toProperties(OperationHistoryDataManagerParams params) {
+        Properties props = new Properties();
+        props.put(Util.ECLIPSE_LINK_KEY_URL, params.getUrl());
+        props.put(Util.ECLIPSE_LINK_KEY_USER, params.getUserName());
+        props.put(Util.ECLIPSE_LINK_KEY_PASS, params.getPassword());
+        props.put(PersistenceUnitProperties.CLASSLOADER, getClass().getClassLoader());
+
+        return props;
+    }
+
+    protected EntityManagerFactory makeEntityManagerFactory(String opsHistPu, Properties props) {
+        return Persistence.createEntityManagerFactory(opsHistPu, props);
+    }
+
+    protected Thread makeThread(EntityManagerFactory emfactory, Consumer<EntityManagerFactory> command) {
+        return new Thread(() -> command.accept(emfactory));
+    }
+}
---
+++ controlloop/common/eventmanager/src/test/java/org/onap/policy/controlloop/eventmanager/EventManagerServicesTest.java
@@ -0,0 +1,119 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP
+ * ================================================================================
+ * Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.controlloop.eventmanager;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Properties;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.onap.policy.common.endpoints.http.client.HttpClientFactoryInstance;
+import org.onap.policy.controlloop.actorserviceprovider.ActorService;
+import org.onap.policy.controlloop.ophistory.OperationHistoryDataManagerImpl;
+import org.onap.policy.controlloop.ophistory.OperationHistoryDataManagerStub;
+import org.onap.policy.drools.utils.PropertyUtil;
+
+public class EventManagerServicesTest {
+    private static final String FILEPFX = "eventService/";
+    private static final IllegalArgumentException EXPECTED_EXCEPTION =
+                    new IllegalArgumentException("expected exception");
+
+    private EventManagerServices services;
+
+    /**
+     * Configures HTTP clients.
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        // start with a clean slate
+        HttpClientFactoryInstance.getClientFactory().destroy();
+
+        Properties props = PropertyUtil.getProperties("src/test/resources/eventService/event-svc-http.properties");
+        HttpClientFactoryInstance.getClientFactory().build(props);
+    }
+
+    @AfterClass
+    public static void teatDownBeforeClass() {
+        HttpClientFactoryInstance.getClientFactory().destroy();
+    }
+
+    @After
+    public void tearDown() {
+        closeDb();
+    }
+
+    @Test
+    public void testEventManagerServices_testGetActorService() {
+        // try with guard disabled - should use DB stub
+        services = new EventManagerServices(FILEPFX + "event-svc-guard-disabled.properties");
+        assertTrue(services.getDataManager() instanceof OperationHistoryDataManagerStub);
+        assertNotNull(services.getActorService());
+
+        // try with guard enabled - should create a DB connection
+        services = new EventManagerServices(FILEPFX + "event-svc-with-db.properties");
+        assertTrue(services.getDataManager() instanceof OperationHistoryDataManagerImpl);
+        assertNotNull(services.getActorService());
+    }
+
+    @Test
+    public void testStartActorService() {
+        // config file not found
+        assertThatIllegalStateException().isThrownBy(() -> new EventManagerServices("missing-config-file"));
+    }
+
+    @Test
+    public void testIsGuardEnabled() {
+        // cannot check guard
+        services = new EventManagerServices(FILEPFX + "event-svc-no-guard-actor.properties");
+        assertTrue(services.getDataManager() instanceof OperationHistoryDataManagerStub);
+
+        // force exception when checking for guard operator
+        services = new EventManagerServices(FILEPFX + "event-svc-with-db.properties") {
+            @Override
+            public ActorService getActorService() {
+                ActorService svc = mock(ActorService.class);
+                when(svc.getActor(any())).thenThrow(EXPECTED_EXCEPTION);
+                return svc;
+            }
+        };
+        assertTrue(services.getDataManager() instanceof OperationHistoryDataManagerStub);
+    }
+
+    @Test
+    public void testMakeDataManager() {
+        assertThatThrownBy(() -> new EventManagerServices(FILEPFX + "event-svc-invalid-db.properties"));
+    }
+
+
+    private void closeDb() {
+        if (services != null) {
+            services.getDataManager().stop();
+        }
+    }
+}
