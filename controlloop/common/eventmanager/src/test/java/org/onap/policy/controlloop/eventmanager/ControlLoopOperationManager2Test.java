33820fc1cf6e3009a7e7216391b48f20335c0e87
Jim Hahn <jrh3@att.com>
2020-06-11 15:33:32 -0400

    Remove deprecated properties for CDS and VFC
    Now that the changes to the Actors have been merged, the former properties
    can be removed, too.
    Also fixed guard operator/operation name since it changed from "Guard"
    to "Decision".
    
    Issue-ID: POLICY-2629
    Change-Id: I1c8d4575be598a8cab12b1aa4ad9b1d3e1f12113
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- controlloop/common/eventmanager/src/test/java/org/onap/policy/controlloop/eventmanager/ControlLoopOperationManager2Test.java
+++ controlloop/common/eventmanager/src/test/java/org/onap/policy/controlloop/eventmanager/ControlLoopOperationManager2Test.java
@@ -57,7 +57,7 @@ import org.onap.policy.controlloop.ControlLoopOperation;
 import org.onap.policy.controlloop.ControlLoopResponse;
 import org.onap.policy.controlloop.VirtualControlLoopEvent;
 import org.onap.policy.controlloop.actor.guard.GuardActorServiceProvider;
-import org.onap.policy.controlloop.actor.guard.GuardOperation;
+import org.onap.policy.controlloop.actor.guard.DecisionOperation;
 import org.onap.policy.controlloop.actorserviceprovider.ActorService;
 import org.onap.policy.controlloop.actorserviceprovider.Operation;
 import org.onap.policy.controlloop.actorserviceprovider.OperationOutcome;
@@ -931,7 +931,7 @@ public class ControlLoopOperationManager2Test {
     private OperationOutcome genGuardOutcome(boolean permit) {
         OperationOutcome outcome = mgr.getParams().makeOutcome();
         outcome.setActor(GuardActorServiceProvider.NAME);
-        outcome.setOperation(GuardOperation.NAME);
+        outcome.setOperation(DecisionOperation.NAME);
         outcome.setStart(Instant.now());
         mgr.getParams().callbackStarted(new OperationOutcome(outcome));
 
