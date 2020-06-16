dd64c4f4eb48af2c85fc4767b8229bf2eeec3773
Taka Cho <takamune.cho@att.com>
2020-06-16 09:42:10 -0400

    fix sonar for m2/base - 2
    - unused import
    - Hide Utility Class Constructor : Utility classes
      should not have a public or default constructor
    
    Issue-ID: POLICY-2616
    Change-Id: I338a739b6e9fb5f4b47898566362b8a87201dae3
    Signed-off-by: Taka Cho <takamune.cho@att.com>

--- controlloop/m2/base/src/main/java/org/onap/policy/m2/base/Util.java
+++ controlloop/m2/base/src/main/java/org/onap/policy/m2/base/Util.java
@@ -20,10 +20,6 @@
 
 package org.onap.policy.m2.base;
 
-import java.util.List;
-
-import org.onap.policy.common.endpoints.event.comm.TopicEndpointManager;
-import org.onap.policy.common.endpoints.event.comm.TopicSink;
 import org.onap.policy.drools.core.PolicyContainer;
 import org.onap.policy.drools.core.PolicySession;
 import org.onap.policy.drools.system.PolicyController;
@@ -33,7 +29,12 @@ import org.onap.policy.drools.system.PolicyEngineConstants;
 /**
  * This class contains static utility methods.
  */
-public class Util {
+public final class Util {
+    // Add a private Util to hide the implicit pubic one.
+    private Util() {
+        //not called
+    }
+
     /**
      * Find the PolicyController associated with the specified PolicySession.
      *
