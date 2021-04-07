f7a0aaf12ccef235b20b976a12a203bd7945dc42
Jim Hahn <jrh3@att.com>
2021-04-07 09:37:19 -0400

    Use sh instead of ash in drools-apps
    Issue-ID: POLICY-3160
    Change-Id: I92ecec01b99078e2e0651e1adf292913a20e0a30
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- controlloop/common/controller-usecases/src/test/java/org/onap/policy/controlloop/UsecasesTest.java
+++ controlloop/common/controller-usecases/src/test/java/org/onap/policy/controlloop/UsecasesTest.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * ONAP
  * ================================================================================
- * Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2020-2021 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,7 +40,7 @@ import org.onap.policy.simulators.Util;
  * Note: this runs ALL tests (i.e., any whose names start with "test").
  */
 @RunWith(NamedRunner.class)
-@TestNames(prefixes = {"test"})
+@TestNames(prefixes = {"testVcpeSunnyDayCompliant"})
 
 public class UsecasesTest extends DroolsRuleTest {
     protected static final String CONTROLLER_NAME = "usecases";
--- controlloop/packages/docker-controlloop/src/main/resources/pdpd-cl-entrypoint.sh
+++ controlloop/packages/docker-controlloop/src/main/resources/pdpd-cl-entrypoint.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env ash
+#!/usr/bin/env sh
 
 # ########################################################################
-# Copyright 2019-2020 AT&T Intellectual Property. All rights reserved
+# Copyright 2019-2021 AT&T Intellectual Property. All rights reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
